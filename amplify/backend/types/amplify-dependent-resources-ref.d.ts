24bbf0f27868b4d6cdd12cdc3fbd5b3207029bbe
suisan <b241651@hiroshima-u.ac.jp>
2025-06-24 15:25:21 +0900

    feat: Add Amplify Auth (Cognito) backend and its config

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,14 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "carpconnect22c256fd8": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "CreatedSNSRole": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
